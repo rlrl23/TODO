16295bae69cff7011fe160072edd3370e2e0cc26
Luibov <rlrl23@mail.ru>
2022-09-21 09:12:07 +0500

    lesson_2

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,18 @@
+const Menu = () => {
+    return (
+        <header>
+            <div class="container">
+                <a href="/" class="logo">LOGO_OF_TODO</a>
+                <nav>
+                    <ul class='nav'>
+                        <li><a href="">Users</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Todo</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Menu
